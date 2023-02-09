48acf5caec7e78359a804539ea692f3ff3c1b1c0
iSiNrus <magnit_322@rambler.ru>
2023-02-09 17:43:01 +0400

    Executors Категории помощи

--- app/src/main/java/ru/barsik/simbirpractic/entity/Category.kt
+++ app/src/main/java/ru/barsik/simbirpractic/entity/Category.kt
@@ -1,6 +1,13 @@
 package ru.barsik.simbirpractic.entity
 
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Category(
     val id : Int,
-    val title : String
-)
+    val title : String,
+    @SerializedName("icon_path")
+    val iconPath: String
+) : Parcelable
