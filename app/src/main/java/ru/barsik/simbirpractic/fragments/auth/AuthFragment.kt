7a2ec33eebd9ca4fd7c573729f57b800a61929e0
iSiNrus <magnit_322@rambler.ru>
2023-02-18 16:16:34 +0400

    Вход по форме авторизации

--- app/src/main/java/ru/barsik/simbirpractic/fragments/auth/AuthFragment.kt
+++ app/src/main/java/ru/barsik/simbirpractic/fragments/auth/AuthFragment.kt
@@ -1,20 +1,45 @@
 package ru.barsik.simbirpractic.fragments.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ru.barsik.simbirpractic.R
+import androidx.fragment.app.Fragment
+import com.jakewharton.rxbinding.widget.RxTextView
+import ru.barsik.simbirpractic.MainActivity
+import ru.barsik.simbirpractic.databinding.FragmentAuthBinding
+import ru.barsik.simbirpractic.fragments.CategoriesFragment
+import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
 
 class AuthFragment : Fragment() {
 
+    private lateinit var binding: FragmentAuthBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_auth, container, false)
+    ): View {
+        binding = FragmentAuthBinding.inflate(layoutInflater)
+        binding.btnSubmit.isEnabled = false
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val observePass = RxTextView.textChanges(binding.etPassword)
+        val observeEmail = RxTextView.textChanges(binding.etEmail)
+
+        Observable.combineLatest(observeEmail, observePass)
+        { x, y ->
+            Pair(x.toString(), y.toString())
+        }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                binding.btnSubmit.isEnabled = (it.first.length >= 6 && it.second.length >= 6)
+            }
+
+        binding.btnSubmit.setOnClickListener {
+            (requireActivity() as MainActivity).switchFragment(CategoriesFragment(), addBackStack = false, showBottomNavigation = true)
+        }
     }
 
 }
