d4ce6dc96f5a949c1e53d4e71d3425984096dbce
iSiNrus <magnit_322@rambler.ru>
2023-03-02 16:57:08 +0400

    Поиск мероприятий

--- app/src/main/java/ru/barsik/simbirpractic/fragments/search/SearchPageEventsFragment.kt
+++ app/src/main/java/ru/barsik/simbirpractic/fragments/search/SearchPageEventsFragment.kt
@@ -1,16 +1,25 @@
 package ru.barsik.simbirpractic.fragments.search
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import ru.barsik.simbirpractic.R
+import ru.barsik.simbirpractic.dao.EventDAO
 import ru.barsik.simbirpractic.databinding.FragmentSearchPageEventsBinding
 
-class SearchPageEventsFragment : Fragment() {
+class SearchPageEventsFragment : Fragment(), SearchableFragment {
 
     private lateinit var binding: FragmentSearchPageEventsBinding
-
+    private var _eventDAO: EventDAO? = null
+    private val eventDAO : EventDAO get() { if(_eventDAO==null) _eventDAO = EventDAO(requireContext())
+        return _eventDAO!!
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,12 +30,28 @@ class SearchPageEventsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        with(binding.recyclerView) {
-//            addItemDecoration(DividerItemDecoration(requireContext(), 1))
-//            layoutManager = LinearLayoutManager(requireContext())
+        with(binding.recyclerView) {
+            addItemDecoration(DividerItemDecoration(requireContext(), 1))
+            layoutManager = LinearLayoutManager(requireContext())
 //            adapter = SearchRecyclerAdapter(
 //                resources.getStringArray(R.array.events_list).also { it.shuffle() })
-//        }
+            adapter = SearchRecyclerAdapter(emptyList())
+        }
+    }
+    override fun setSearchQuery(query: String) {
+        try {
+            val resList = eventDAO.getEvents().filter { x -> x.title.contains(query, true) }
+            if (resList.isEmpty()) {
+                binding.searchContent.isVisible = false
+                binding.clPlaceholder.isVisible = true
+            } else {
+                binding.searchContent.isVisible = true
+                binding.clPlaceholder.isVisible = false
+                (binding.recyclerView.adapter as SearchRecyclerAdapter).setData(resList)
+            }
+        } catch (e: Exception){
+            Log.e("TAG", "setSearchQuery: CHPOK!", )
+        }
     }
 
 }
